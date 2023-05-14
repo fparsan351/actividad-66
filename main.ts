815a68b264ddcd704565cdaad3628ca8f02345b0
fparsan351 <132547811+fparsan351@users.noreply.github.com>
2023-05-14 12:57:00 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(100)
+    basic.showIcon(IconNames.Confused)
+    basic.pause(100)
+    basic.showIcon(IconNames.Tortoise)
+    basic.pause(100)
+    basic.showIcon(IconNames.House)
+    basic.pause(100)
+    basic.showIcon(IconNames.Confused)
+    basic.pause(100)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+}
